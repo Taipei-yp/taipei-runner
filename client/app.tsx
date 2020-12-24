d1a5fa0becbde70e9275ddfb6a2d152ef0b8b1ed
rs <redin.s.o@yandex.ru>
2020-12-25 00:48:40 +0300

    added routes without components

--- client/app.tsx
+++ client/app.tsx
@@ -1,5 +1,26 @@
 import React, { FC } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import { SignUp } from "./pages/signup";
 
 export const App: FC = () => {
-  return <div>App Test</div>;
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/signin" />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/profile" />
+          <Route path="/leaderboard" />
+          <Route path="/forum" />
+          <Route path="/forum/topic/:id" />
+          <Route path="/feedback" />
+          <Route path="/game" />
+          <Route path="/game-over" />
+          <Route exact path="/" />
+          <Route path="/*" />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
