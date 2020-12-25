9cf9739cc4c733a901af0aa06cd90a8758862342
rs <redin.s.o@yandex.ru>
2020-12-25 13:01:58 +0300

    add default avatar

--- client/components/avatar/avatar.tsx
+++ client/components/avatar/avatar.tsx
@@ -1,6 +1,8 @@
 import React, { FC, memo } from "react";
 import block from "bem-cn";
 
+import defaultAvatar from "../../assets/images/default-avatar.png";
+
 import "./button.css";
 
 const b = block("avatar");
@@ -12,7 +14,12 @@ export type Props = {
   alt: string;
 };
 
-const Avatar: FC<Props> = ({ size = null, className = "", alt = "", src }) => {
+const Avatar: FC<Props> = ({
+  size = null,
+  className = "",
+  alt = "",
+  src = defaultAvatar,
+}) => {
   return (
     <div className={b({ size }).mix(className)}>
       <img src={src} className={b("image")} alt={alt} />
