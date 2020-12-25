7301f84bd139789ebf46bbf0956fc5064247386f
rs <redin.s.o@yandex.ru>
2020-12-25 13:13:02 +0300

    перенес роуты в app

--- client/app.tsx
+++ client/app.tsx
@@ -1,12 +1,25 @@
 import React, { FC } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Routing } from "./components/routing";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import { SignUp } from "./pages/signup";
 
 export const App: FC = () => {
   return (
     <div className="app">
       <Router>
-        <Routing />
+        <Switch>
+          <Route exact path="/" />
+          <Route path="/signin" />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/profile" />
+          <Route path="/leaderboard" />
+          <Route path="/forum" />
+          <Route path="/forum/topic/:id" />
+          <Route path="/feedback" />
+          <Route path="/game" />
+          <Route path="/game-over" />
+          <Route path="*" />
+        </Switch>
       </Router>
     </div>
   );
