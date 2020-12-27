79041c032f506ea42c9e5bc5efb2df7f03d0497f
Susam <susam.buildings@gmail.com>
2020-12-27 17:45:31 +0300

    add forum page

--- client/components/table/table-sort.ts
+++ client/components/table/table-sort.ts
@@ -9,39 +9,32 @@ export type SortDirection = "asc" | "desc";
 
 type SortRequestHandler = (field: string) => void;
 
+const nextSortDirection: Record<
+  SortDirection | "null",
+  SortDirection | null
+> = {
+  null: "asc",
+  asc: "desc",
+  desc: null,
+};
+
 export function useNullableTableSort(): [TableSort, SortRequestHandler] {
-  const [sort, setSort] = useState({
+  const [{ field, direction }, setSort] = useState({
     field: null,
     direction: null,
   } as TableSort);
 
-  const handleSortRequest = useCallback((field: string) => {
-    // updating sort: null -> asc -> desc -> null -> asc -> ...
-    setSort(({ field: prevField, direction: prevDirection }) => {
-      if (prevField) {
-        if (prevField === field && prevDirection === "asc") {
-          return {
-            field,
-            direction: "asc",
-          };
-        }
+  const handleSortRequest = useCallback(
+    (sortBy: string) => {
+      const newSortDirection =
+        nextSortDirection[String(direction) as SortDirection | "null"];
+      const newSortField = newSortDirection ? sortBy : null;
+      setSort({ field: newSortField, direction: newSortDirection });
+    },
+    [direction],
+  );
 
-        if (prevField === field && prevDirection === "desc") {
-          return {
-            field: null,
-            direction: null,
-          };
-        }
-      }
-
-      return {
-        field,
-        direction: "asc",
-      };
-    });
-  }, []);
-
-  return [sort, handleSortRequest];
+  return [{ field, direction }, handleSortRequest];
 }
 
 // we can add other variants here, maybe
