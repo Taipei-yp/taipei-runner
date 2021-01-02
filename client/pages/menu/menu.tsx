0a2cfbf566de497b5384af733a1d3cc59636f8b2
kyponomarev <35010068+kyponomarev@users.noreply.github.com>
2021-01-02 08:00:46 +0300

    Sprint_1.task_3.5: add menu page (#63)

---
+++ client/pages/menu/menu.tsx
@@ -0,0 +1,57 @@
+import React, { FC, memo } from "react";
+import block from "bem-cn";
+import { Heading } from "../../components/heading";
+import { Page } from "../../components/page";
+import { LinkView } from "../../components/link-view";
+
+import "./menu.css";
+
+const b = block("menu");
+
+type Props = {
+  className?: string;
+};
+
+const MenuItems = [
+  {
+    to: "/game",
+    label: "Start Game",
+  },
+  {
+    to: "/profile",
+    label: "Profile",
+  },
+  {
+    to: "/leaderboard",
+    label: "Leaderboard",
+  },
+  {
+    to: "/forum",
+    label: "Forum",
+  },
+];
+
+const Menu: FC<Props> = ({ className = "" }) => {
+  return (
+    <Page fixHeader fullHeight>
+      <div className={b.mix(className)}>
+        <Heading
+          color="primary"
+          text="Taipei Runner"
+          size="l"
+          className={b("heading")}
+        />
+        <ul className={b("list")}>
+          {MenuItems.map(({ label, to }) => (
+            <li className={b("item")} key={label}>
+              <LinkView to={to} label={label} size="xxl" />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Page>
+  );
+};
+
+const WrappedMenu = memo(Menu);
+export { WrappedMenu as Menu };
