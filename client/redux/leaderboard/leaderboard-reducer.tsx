9838ad3056ad68e25845749b0d4fe358f0c8cf47
rdnsr <71991203+rdnsr@users.noreply.github.com>
2021-01-18 12:22:53 +0300

    Sprint 2 Task 12,13 redux, redux-thunk, reselect
    * add redux; redux catalog;
    
    * initiastate in reduces files
    
    * add redux-thunk and reselect

---
+++ client/redux/leaderboard/leaderboard-reducer.tsx
@@ -0,0 +1,7 @@
+const initialState = {
+  leaderboard: {},
+};
+// TODO Поменять тип переменной state
+export const leaderboardReducer = (state: unknown = initialState) => {
+  return state;
+};
---
+++ client/redux/user/user-reducer.tsx
@@ -0,0 +1,7 @@
+const initialState = {
+  user: {},
+};
+// TODO Поменять тип переменной state
+export const userReducer = (state: unknown = initialState) => {
+  return state;
+};
