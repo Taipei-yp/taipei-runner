15987f5376e98fa4ca1f2ce65263dffbf81c31bf
rs <redin.s.o@yandex.ru>
2021-01-25 02:55:50 +0300

    move jest files to folder

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   notify: true,
   verbose: true,
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/jest.fileMock.config.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/config/jest.fileMock.js",
     "\\.(css|scss)$": "identity-obj-proxy",
   },
   transform: {
@@ -18,5 +18,5 @@ module.exports = {
     },
   },
   modulePaths:["<rootDir>/"],
-  snapshotResolver: "<rootDir>/jest.snapshotResolver.config.js"
+  snapshotResolver: "<rootDir>/config/jest.snapshotResolver.js"
 };
