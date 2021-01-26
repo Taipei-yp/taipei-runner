7fe2699d6a27f0ed85a4b5f64fe9978bed6dee65
rdnsr <71991203+rdnsr@users.noreply.github.com>
2021-01-26 14:37:41 +0300

    Snapshot tests (#110)
    * snapshot tests
    
    * fix warning

---
+++ client/components/form-field/form-field.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TestRenderer from "react-test-renderer";
+import { FormField } from ".";
+
+describe("Component FormField", () => {
+  const formField = TestRenderer.create(
+    <FormField labelText="test">
+      <input type="text" />
+    </FormField>,
+  );
+
+  it("correct render", () => {
+    expect(formField.toJSON()).toMatchSnapshot();
+  });
+});
