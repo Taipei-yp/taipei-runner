c2368882496076836ff484ea29c3759cea54ffc3
Kirill Ponomarev <ky.ponomarev@yandex.ru>
2021-02-26 17:29:16 +0300

    add context to score-counter test

--- client/components/score-counter/score-counter.test.tsx
+++ client/components/score-counter/score-counter.test.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Provider } from "react-redux";
 import TestRenderer from "react-test-renderer";
+import { configureStore } from "client/redux/store";
 import { ScoreCounter } from ".";
 
 describe("Component ScoreCounter", () => {
-  const scoreCounter = TestRenderer.create(<ScoreCounter score={1000} />);
+  const { store } = configureStore();
+
+  const scoreCounter = TestRenderer.create(
+    <Provider store={store}>
+      <ScoreCounter score={1000} />
+    </Provider>,
+  );
 
   it("correct render", () => {
     expect(scoreCounter.toJSON()).toMatchSnapshot();
