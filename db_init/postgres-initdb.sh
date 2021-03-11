50957feb827b1b0244a0340f045f46dc470ac99d
rs <redin.s.o@yandex.ru>
2021-03-12 01:41:31 +0300

    docker compose fix

--- db_init/postgres-initdb.sh
+++ db_init/postgres-initdb.sh
@@ -1,7 +1,7 @@
-  #!/bin/sh -e
+#!/bin/sh -e
 
 psql --variable=ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
     CREATE ROLE runner WITH LOGIN PASSWORD 'runner';
-    CREATE DATABASE "taipei_runner" OWNER = runner;
-    GRANT ALL PRIVILEGES ON DATABASE "taipei_runner" TO runner;
+    CREATE DATABASE "taipei-runner" OWNER = runner;
+    GRANT ALL PRIVILEGES ON DATABASE "taipei-runner" TO runner;
 EOSQL
