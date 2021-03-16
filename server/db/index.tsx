d0f49261f91b9531ce805d46879aa633544fc92f
rs <redin.s.o@yandex.ru>
2021-03-16 16:31:32 +0300

    initial data

--- server/db/index.tsx
+++ server/db/index.tsx
@@ -1,6 +1,12 @@
 import { mongoConnection, psqlConnection } from "./connections";
+import { createSiteThemes } from "./initial-data";
 
 async function initDadabases() {
-  return Promise.allSettled([psqlConnection.sync(), mongoConnection.connect()]);
+  return Promise.allSettled([
+    psqlConnection.sync().then(async () => {
+      return createSiteThemes();
+    }),
+    mongoConnection.connect(),
+  ]);
 }
 export { initDadabases, mongoConnection, psqlConnection };
--- server/db/initial-data.tsx
+++ server/db/initial-data.tsx
@@ -1,10 +1,24 @@
-export const SiteThemesData = [
-  {
-    theme: "dark",
-    description: "dark theme",
-  },
-  {
-    theme: "light",
-    description: "light theme",
-  },
-];
+import SiteTheme from "./schemes/site-theme";
+
+const createSiteThemes = async () => {
+  await SiteTheme.findOrCreate({
+    where: {
+      theme: "dark",
+    },
+    defaults: {
+      description: "dark theme",
+      deleted: false,
+    },
+  });
+  await SiteTheme.findOrCreate({
+    where: {
+      theme: "light",
+    },
+    defaults: {
+      description: "light theme",
+      deleted: false,
+    },
+  });
+};
+
+export { createSiteThemes };
