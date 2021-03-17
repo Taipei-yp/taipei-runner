85ce4e61e8cd7a467608d3abb20339569a5265b8
rs <redin.s.o@yandex.ru>
2021-03-17 23:50:45 +0300

    Z

--- client/enviroment.ts
+++ client/enviroment.ts
@@ -3,12 +3,11 @@ const environment = {
   description: "Pixel runner",
   apiUrl: "https://ya-praktikum.tech/api/v2",
   uploadsUrl: "https://ya-praktikum.tech",
-  localApiUrl: `${
+  localApiUrl:
     process.env.API_HOST ||
     (process.env.NODE_ENV !== "production"
-      ? "https://local.ya-praktikum.tech:4000"
-      : "https://taipei-runner-02.ya-praktikum.tech")
-  }/api`,
+      ? "https://local.ya-praktikum.tech:4000/api"
+      : "https://taipei-runner-02.ya-praktikum.tech/api"),
 };
 
 export { environment };
