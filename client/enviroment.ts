a4c91a503f5992140e1964f0ed176a3fb48bbecd
rs <redin.s.o@yandex.ru>
2021-03-18 00:41:38 +0300

    G

--- client/enviroment.ts
+++ client/enviroment.ts
@@ -4,7 +4,9 @@ const environment = {
   apiUrl: "https://ya-praktikum.tech/api/v2",
   uploadsUrl: "https://ya-praktikum.tech",
   localApiUrl:
-    process.env.API_HOST || "https://local.ya-praktikum.tech:4000/api",
+    (process.env.API_HOST || "development") !== "production"
+      ? "https://local.ya-praktikum.tech:4000/api"
+      : "https://taipei-runner-02.ya-praktikum.tech/api",
 };
 
 export { environment };
